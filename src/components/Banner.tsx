6e0329cf217187c9e4b74aa96cf01308a7398ab6
Tuckkrub00 <pantana2004@gmail.com>
2024-03-16 10:46:22 +0700

    Assignment07 Submission

---
+++ src/components/Banner.tsx
@@ -0,0 +1,31 @@
+'use client'
+import { useState } from 'react';
+import styles from'./banner.module.css'
+import Image from 'next/image';
+import {useRouter} from 'next/navigation'
+export default function Banner () {
+    const cover = ['/img/cover.jpg','/img/cover2.jpg','/img/cover3.jpg']
+    const [index, setIndex] = useState(0)
+    const router = useRouter()
+    return (
+        <div className={styles.banner} onClick={()=>{ setIndex(index+1)}}>
+            <Image src={cover[index%3]}
+            alt='cover'
+            fill={true}
+            priority
+             objectFit='cover'
+            />
+            <div className={styles.bannerText}>
+                <h1 className='text-4xl font-medium'>Your Travel Partner</h1>
+                <h3 className='text-xl font-serif'>Explore Your World with us</h3>
+            </div>
+            <button className='bg-white text-cyan-600 border border-cyan-600
+            font-semibold py-2 px-2 m-2 rounded z-30 absolute bottom-0 right-0
+            hover:bg-cyan-600 hover:text-white hover:border-transparent'
+            onClick={(e)=>{e.stopPropagation(); router.push('/car')}}
+            >
+                Select Your Travel Parner NOW
+            </button>
+        </div>
+    );
+}
